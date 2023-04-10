ddd48dfa53c0f16cd4d2b5362ba3abe80cdf8830
Devrex0676 <113067167+Devrex0676@users.noreply.github.com>
2023-04-09 22:32:55 -0700

    Added files via upload
    Added all the files for this project.

---
+++ index.js
@@ -0,0 +1,62 @@
+window.addEventListener("load", function() {
+  var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+  var img1 = document.querySelector("img.img1");
+  var img2 = document.querySelector("img.img2");
+
+
+  switch (randomnumber1) {
+    case 1:
+      img1.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice1.png");
+      break;
+    case 2:
+      img1.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice2.png");
+      break;
+    case 3:
+      img1.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice3.png");
+      break;
+    case 4:
+      img1.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice4.png");
+      break;
+    case 5:
+      img1.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice5.png");
+      break;
+    case 6:
+      img1.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice6.png");
+      break;
+    default:
+      break;
+  }
+
+  switch (randomnumber2) {
+    case 1:
+      img2.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice2.png");
+      break;
+    case 2:
+      img2.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice2.png");
+      break;
+    case 3:
+      img2.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice3.png");
+      break;
+    case 4:
+      img2.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice4.png");
+      break;
+    case 5:
+      img2.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice5.png");
+      break;
+    case 6:
+      img2.setAttribute("src", "C:\\Users\\deadl\\Documents\\Developer\\Class\\Dice Game\\images\\dice6.png");
+      break;
+    default:
+      break;
+  }
+
+  if (randomnumber1 > randomnumber2) {
+    document.querySelector(".MyBigHeader").textContent = "Player 1 Wins";
+  } else if (randomnumber1 < randomnumber2) {
+    document.querySelector(".MyBigHeader").textContent = "Player 2 Wins";
+  } else {
+    document.querySelector(".MyBigHeader").textContent = "Draw";
+  }
+  
+});
